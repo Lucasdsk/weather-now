15f96ad4364b4e0b67a4154a03c403b058ea44b6
Lucasdsk <lucasgomes.dky@gmail.com>
2018-06-23 18:23:43 -0300

    chart added and customized

--- src/controllers/Main.js
+++ src/controllers/Main.js
@@ -1,11 +1,73 @@
+import Chart from 'chart.js';
+import datalabels from 'chartjs-plugin-datalabels';
+
 class MainController {
   constructor() {
     this.name = 'Weather Now';
   }
 
-  init() {
-    document.getElementById('app-name').innerHTML = this.name;
-  }
+  init = () => {
+    const chartElement = document.getElementById('myChart');
+    new Chart(chartElement, {
+      type: 'line',
+      data: {
+        labels: ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6'],
+        datasets: [
+          {
+            label: '',
+            data: [12, 19, 3, 5, 2, 3],
+            borderColor: '#c7e8f3',
+            fill: true,
+            backgroundColor: 'rgba(199, 232, 243, 0.34)',
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        layout: {
+          padding: {
+            top: 20,
+          },
+        },
+        legend: {
+          display: false,
+        },
+        tooltips: {
+          displayColors: false,
+        },
+        scales: {
+          xAxes: [
+            {
+              ticks: {
+                fontColor: '#c7e8f3',
+              },
+              gridLines: {
+                display: false,
+              },
+            },
+          ],
+
+          yAxes: [
+            {
+              ticks: {
+                fontColor: '#c7e8f3',
+              },
+              gridLines: {
+                display: false,
+              },
+            },
+          ],
+        },
+        plugins: {
+          datalabels: {
+            borderRadius: 4,
+            color: 'white',
+            align: 'top',
+          },
+        },
+      },
+    });
+  };
 }
 
 export default new MainController();
